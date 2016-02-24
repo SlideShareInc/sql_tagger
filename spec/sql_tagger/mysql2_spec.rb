68d823a65ccde3164016c608f8d90762c8a30c07
Toby Hsieh <thrasher13b@gmail.com>
2016-02-23 18:52:22 -0800

    Use expect syntax instead of `should_receive`

--- spec/sql_tagger/mysql2_spec.rb
+++ spec/sql_tagger/mysql2_spec.rb
@@ -19,8 +19,10 @@ describe Mysql2::Client do
     end
 
     it 'calls SqlTagger#tag' do
-      @db.sql_tagger.should_receive(:tag).and_return('/* something.rb */ SELECT 1')
-      @db.query('SELECT 1')
+      query = 'SELECT 1'
+      expect(@db.sql_tagger).to receive(:tag).with(query).
+        and_return("/* something.rb */ #{query}")
+      @db.query(query)
     end
   end
 end
